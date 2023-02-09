2ed1801af9c7ee50ca3830af046fb6352577c0b3
Camilo <betancurcamilo50@gmail.com>
2023-02-08 22:03:04 -0500

    se dañó el section Uno y se termino parte del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,33 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className='Contenedor-footer-contactos'>
-            <div>
-                <h4>Categorias</h4>
-                <li>
-                    <ul>Hombre</ul>
-                    <ul>Mujer</ul>
-                    <ul>Ropa</ul>
-                </li>
+            <div className='Categorias'>
+                <h4 className='categorias'>CATEGORIAS</h4>
+                <ul>
+                    <li> - Hombre</li>
+                    <li> - Mujer</li>
+                    <li> - Ropa</li>
+                </ul>
+            </div>
+
+            <div className='Informacion-contacto'>
+                <div className='contenedor-contacto'>
+                        <h4>CONTACTO</h4>
+                        <p>Itaguí</p>
+                        <p>3175499226</p>
+                        <p>betancurcamilo50@gmail.com</p>
+                </div>
+                
+                <div className='iconos'>
+                    <a href="https://www.linkedin.com/in/camilo-betancur-1a2023246/" class="fa fa-linkedin"> </a>
+                    <a href="linkInstagram" class="fa fa-instagram"> </a>
+                    <a href="https://wa.me/573175499226" class="fa fa-whatsapp"> </a>
+                </div>
+            </div>
+
+            <div className='formulario'>
+              <h4>SUSCRIBETE</h4>
+                
             </div>
         
         </div>
